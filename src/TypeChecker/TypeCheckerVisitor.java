3039bd76681fa79cbb7cb9a87eff0fd36486f4bb
Simonbolsen <Simonbolsen1234@gmail.com>
2022-09-27 10:58:20 +0200

    Update TypeCheckerVisitor.java

--- src/TypeChecker/TypeCheckerVisitor.java
+++ src/TypeChecker/TypeCheckerVisitor.java
@@ -7,22 +7,27 @@ public class TypeCheckerVisitor extends UCELBaseVisitor<Type> {
         Type.TypeEnum leftType = visit(ctx.children.get(0)).getEvaluationType();
         Type.TypeEnum rightType = visit(ctx.children.get(1)).getEvaluationType();
 
+        return intDoubleBinaryOp(leftType, rightType);
+    }
+
+    private Type intDoubleBinaryOp(Type.TypeEnum leftType, Type.TypeEnum rightType) {
         if(leftType == Type.TypeEnum.errorType ||
-            rightType == Type.TypeEnum.errorType) {
+                rightType == Type.TypeEnum.errorType) {
             return new Type(Type.TypeEnum.errorType);
         } else if((leftType == Type.TypeEnum.intType ||
                 leftType == Type.TypeEnum.doubleType) &&
                 rightType == Type.TypeEnum.intType) {
             return new Type(leftType);
         } else if(rightType == Type.TypeEnum.doubleType &&
-                    (leftType == Type.TypeEnum.intType ||
-                    leftType == Type.TypeEnum.doubleType)) {
+                (leftType == Type.TypeEnum.intType ||
+                        leftType == Type.TypeEnum.doubleType)) {
             return new Type(rightType);
         } else {
             //TODO logger
             return new Type(Type.TypeEnum.errorType);
         }
-
     }
+
+
 
 }
