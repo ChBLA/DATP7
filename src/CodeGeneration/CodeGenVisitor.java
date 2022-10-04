afc69f4ee4837704eaa98445909a6bafc86c40d1
DEEZ <stran19@student.aau.dk>
2022-10-04 11:28:53 +0200

    code gen add test

--- src/CodeGeneration/CodeGenVisitor.java
+++ src/CodeGeneration/CodeGenVisitor.java
@@ -1,4 +1,10 @@
 import java.util.ArrayList;
 
 public class CodeGenVisitor extends UCELBaseVisitor<ArrayList<Template>> {
+
+    @Override
+    public ArrayList<Template> visitAddSub(UCELParser.AddSubContext ctx) {
+
+        return super.visitAddSub(ctx);
+    }
 }
