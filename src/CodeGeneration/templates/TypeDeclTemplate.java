a45229e5eb849b9d4589508ed4130bae8c2c582d
DEEZ <stran19@student.aau.dk>
2022-10-11 11:06:20 +0200

    typedecl template added

--- src/CodeGeneration/templates/TypeDeclTemplate.java
+++ src/CodeGeneration/templates/TypeDeclTemplate.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class TypeDeclTemplate {
+import java.util.ArrayList;
+import java.util.List;
+
+public class TypeDeclTemplate implements Template {
+    String result;
+
+    public TypeDeclTemplate(Template type, List<Template> arrayDeclIDs) {
+        result = String.format("typedef %s ", type.getOutput());
+
+        for (var arrayDeclIDsItem : arrayDeclIDs) {
+            result += arrayDeclIDsItem.getOutput() + ", ";
+        }
+
+        result = result.replaceFirst(", $", ";");
+    }
+
+    @Override
+    public String getOutput() {
+        return result;
+    }
 }
