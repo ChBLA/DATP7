3199e6004a17108efff6c18fa17c45202df8ec1f
Henrik Ginnerup <hginnerup@hotmail.com>
2022-10-15 09:39:33 +0200

    Added Location base class

---
+++ UppaalInterface/src/main/java/org/Ucel/Location.java
@@ -0,0 +1,135 @@
+package org.Ucel;
+
+public class Location implements IGraph.ILocation {
+    private String id = "";
+
+    @Override
+    public String GetId() {
+        return id;
+    }
+
+    public void SetId(String value) {
+        id = value;
+    }
+
+    private int posX = 0;
+
+    @Override
+    public int GetPosX() {
+        return posX;
+    }
+
+    public void SetPosX(int value) {
+        posX = value;
+    }
+
+    private int posY = 0;
+
+    @Override
+    public int GetPosY() {
+        return posY;
+    }
+
+    public void SetPosY(int value) {
+        posY = value;
+    }
+
+    private String name = "";
+
+    @Override
+    public String GetName() {
+        return name;
+    }
+
+    public void SetName(String value) {
+        name = value;
+    }
+
+    private String invariant = "";
+
+    @Override
+    public String GetInvariant() {
+        return invariant;
+    }
+
+    public void SetInvariant(String value) {
+        invariant = value;
+    }
+
+    private String rateOfExponential = "";
+
+    @Override
+    public String GetRateOfExponential() {
+        return rateOfExponential;
+    }
+
+    public void SetRateOfExponential(String value) {
+        rateOfExponential = value;
+    }
+
+    private boolean initial = false;
+
+    @Override
+    public boolean GetInitial() {
+        return initial;
+    }
+
+    public void SetInitial(boolean value) {
+        initial = value;
+    }
+
+    private boolean urgent = false;
+
+    @Override
+    public boolean GetUrgent() {
+        return urgent;
+    }
+
+    public void SetUrgent(boolean value) {
+        urgent = value;
+    }
+
+    private boolean committed = false;
+
+    @Override
+    public boolean GetCommitted() {
+        return committed;
+    }
+
+    public void SetCommitted(boolean value) {
+        committed = value;
+    }
+
+    private String comments = "";
+
+    @Override
+    public String GetComments() {
+        return comments;
+    }
+
+    public void SetComments(String value) {
+        comments = value;
+    }
+
+    private String testCodeOnEnter = "";
+
+    @Override
+    public String GetTestCodeOnEnter() {
+        return testCodeOnEnter;
+    }
+
+    public void SetTestCodeOnEnter(String value) {
+        testCodeOnEnter = value;
+    }
+
+    private String testCodeOnExit = "";
+
+    @Override
+    public String GetTestCodeOnExit() {
+        return testCodeOnExit;
+    }
+
+    public void SetTestCodeOnExit(String value) {
+        testCodeOnExit = value;
+    }
+}
---
+++ UppaalInterface/src/test/java/LocationTests.java
@@ -0,0 +1,127 @@
+import org.Ucel.Edge;
+import org.Ucel.IGraph;
+import org.Ucel.Location;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class LocationTests {
+    @Test
+    public void ctor() {
+        IGraph.ILocation location = new Location();
+
+        assertNotNull(location.GetId());
+        assertNotNull(location.GetPosX());
+        assertNotNull(location.GetPosY());
+        assertNotNull(location.GetName());
+        assertNotNull(location.GetInvariant());
+        assertNotNull(location.GetRateOfExponential());
+        assertNotNull(location.GetInitial());
+        assertNotNull(location.GetUrgent());
+        assertNotNull(location.GetCommitted());
+
+        assertNotNull(location.GetComments());
+        assertNotNull(location.GetTestCodeOnEnter());
+        assertNotNull(location.GetTestCodeOnExit());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"glkdgklfjd", "lkjdgjlkdf"})
+    public void Id(String value) {
+        Location location = new Location();
+        location.SetId(value);
+        assertEquals(value, location.GetId());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {34, 745})
+    public void PosX(int value) {
+        Location location = new Location();
+        location.SetPosX(value);
+        assertEquals(value, location.GetPosX());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {34, 745})
+    public void PosY(int value) {
+        Location location = new Location();
+        location.SetPosY(value);
+        assertEquals(value, location.GetPosY());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"glkdgklfjd", "lkjdgjlkdf"})
+    public void Name(String value) {
+        Location location = new Location();
+        location.SetName(value);
+        assertEquals(value, location.GetName());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"glkdgklfjd", "lkjdgjlkdf"})
+    public void Invariant(String value) {
+        Location location = new Location();
+        location.SetInvariant(value);
+        assertEquals(value, location.GetInvariant());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"glkdgklfjd", "lkjdgjlkdf"})
+    public void RateOfExponential(String value) {
+        Location location = new Location();
+        location.SetRateOfExponential(value);
+        assertEquals(value, location.GetRateOfExponential());
+    }
+
+    @ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    public void Initial(boolean value) {
+        Location location = new Location();
+        location.SetInitial(value);
+        assertEquals(value, location.GetInitial());
+    }
+
+    @ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    public void Urgent(boolean value) {
+        Location location = new Location();
+        location.SetUrgent(value);
+        assertEquals(value, location.GetUrgent());
+    }
+
+    @ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    public void Committed(boolean value) {
+        Location location = new Location();
+        location.SetCommitted(value);
+        assertEquals(value, location.GetCommitted());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"glkdgklfjd", "lkjdgjlkdf"})
+    public void Comments(String value) {
+        Location location = new Location();
+        location.SetComments(value);
+        assertEquals(value, location.GetComments());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"glkdgklfjd", "lkjdgjlkdf"})
+    public void TestCodeOnEnter(String value) {
+        Location location = new Location();
+        location.SetTestCodeOnEnter(value);
+        assertEquals(value, location.GetTestCodeOnEnter());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"glkdgklfjd", "lkjdgjlkdf"})
+    public void TestCodeOnExit(String value) {
+        Location location = new Location();
+        location.SetTestCodeOnExit(value);
+        assertEquals(value, location.GetTestCodeOnExit());
+    }
+
+}
