4804d968b8f22f53973eaa0fc0c5a686291466b5
Henrik Ginnerup <hginnerup@hotmail.com>
2022-10-15 09:12:46 +0200

    Added Edge base implementation

---
+++ UppaalInterface/src/main/java/org/Ucel/Edge.java
@@ -0,0 +1,102 @@
+package org.Ucel;
+
+public class Edge implements IGraph.IEdge {
+    private String id = "";
+
+    @Override
+    public String GetId() {
+        return id;
+    }
+
+    public void SetId(String value) {
+        id = value;
+    }
+
+    private String locationIdStart = "";
+
+    @Override
+    public String GetLocationIdStart() {
+        return locationIdStart;
+    }
+
+    public void SetLocationIdStart(String value) {
+        locationIdStart = value;
+    }
+
+    private String locationIdEnd = "";
+
+    @Override
+    public String GetLocationIdEnd() {
+        return locationIdEnd;
+    }
+
+    public void SetLocationIdEnd(String value) {
+        locationIdEnd = value;
+    }
+
+    private String select = "";
+
+    @Override
+    public String GetSelect() {
+        return select;
+    }
+
+    public void SetSelect(String value) {
+        select = value;
+    }
+
+    private String guard = "";
+
+    @Override
+    public String GetGuard() {
+        return guard;
+    }
+
+    public void SetGuard(String value) {
+        guard = value;
+    }
+
+    private String sync = "";
+
+    @Override
+    public String GetSync() {
+        return sync;
+    }
+
+    public void SetSync(String value) {
+        sync = value;
+    }
+
+    private String update = "";
+
+    @Override
+    public String GetUpdate() {
+        return update;
+    }
+
+    public void SetUpdate(String value) {
+        update = value;
+    }
+
+    private String comment = "";
+
+    @Override
+    public String GetComment() {
+        return comment;
+    }
+
+    public void SetComment(String value) {
+        comment = value;
+    }
+
+    private String testCode = "";
+
+    @Override
+    public String GetTestCode() {
+        return testCode;
+    }
+
+    public void SetTestCode(String value) {
+        testCode = value;
+    }
+}
---
+++ UppaalInterface/src/test/java/EdgeTests.java
@@ -0,0 +1,98 @@
+import org.Ucel.Edge;
+import org.Ucel.IGraph;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class EdgeTests {
+    @Test
+    public void ctor() {
+        IGraph.IEdge edge = new Edge();
+
+        assertNotNull(edge.GetId());
+        assertNotNull(edge.GetLocationIdStart());
+        assertNotNull(edge.GetLocationIdEnd());
+        assertNotNull(edge.GetSelect());
+        assertNotNull(edge.GetGuard());
+        assertNotNull(edge.GetSync());
+        assertNotNull(edge.GetUpdate());
+        assertNotNull(edge.GetComment());
+        assertNotNull(edge.GetTestCode());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"sdfgsfdghhsf", "hsdfghfdg"})
+    public void Id(String value) {
+        Edge edge = new Edge();
+
+        edge.SetId(value);
+        assertEquals(value, edge.GetId());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"sdfgsfdghhsf", "hsdfghfdg"})
+    public void LocationIdStart(String value) {
+        Edge edge = new Edge();
+
+        edge.SetLocationIdStart(value);
+        assertEquals(value, edge.GetLocationIdStart());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"sdfgsfdghhsf", "hsdfghfdg"})
+    public void LocationIdEnd(String value) {
+        Edge edge = new Edge();
+
+        edge.SetLocationIdEnd(value);
+        assertEquals(value, edge.GetLocationIdEnd());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"sdfgsfdghhsf", "hsdfghfdg"})
+    public void Guard(String value) {
+        Edge edge = new Edge();
+
+        edge.SetGuard(value);
+        assertEquals(value, edge.GetGuard());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"sdfgsfdghhsf", "hsdfghfdg"})
+    public void Sync(String value) {
+        Edge edge = new Edge();
+
+        edge.SetSync(value);
+        assertEquals(value, edge.GetSync());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"sdfgsfdghhsf", "hsdfghfdg"})
+    public void Update(String value) {
+        Edge edge = new Edge();
+
+        edge.SetUpdate(value);
+        assertEquals(value, edge.GetUpdate());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"sdfgsfdghhsf", "hsdfghfdg"})
+    public void Comment(String value) {
+        Edge edge = new Edge();
+
+        edge.SetComment(value);
+        assertEquals(value, edge.GetComment());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"sdfgsfdghhsf", "hsdfghfdg"})
+    public void TestCode(String value) {
+        Edge edge = new Edge();
+
+        edge.SetTestCode(value);
+        assertEquals(value, edge.GetTestCode());
+    }
+
+}
