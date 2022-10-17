7c1084cf63349bfbaeef3774716a595ead8659b1
Henrik Ginnerup <hginnerup@hotmail.com>
2022-10-17 10:54:42 +0200

    Added tests for Template

--- UppaalInterface/src/main/java/org/Ucel/Template.java
+++ UppaalInterface/src/main/java/org/Ucel/Template.java
@@ -1,23 +1,48 @@
 package org.Ucel;
 
 public class Template implements ITemplate {
+
+    private String name = "";
+
     @Override
     public String getName() {
-        return null;
+        return name;
     }
+
+    public void setName(String value) {
+        name = value;
+    }
+
+    private String parameters = "";
 
     @Override
     public String getParameters() {
-        return null;
+        return parameters;
     }
+
+    public void setParameters(String value) {
+        parameters = value;
+    }
+
+    private IGraph graph = new Graph();
 
     @Override
     public IGraph getGraph() {
-        return null;
+        return graph;
     }
+
+    public void setGraph(IGraph value) {
+        graph = value;
+    }
+
+    private String declarations = "";
 
     @Override
     public String getDeclarations() {
-        return null;
+        return declarations;
+    }
+
+    public void setDeclarations(String value) {
+        declarations = value;
     }
 }
