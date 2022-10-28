c0e2598918d3ccbae883488b58249c4dea92d37a
ChBLA <cbla19@student.aau.dk>
2022-10-28 10:21:59 +0200

    Added class for manual parser

---
+++ compiler/src/main/java/org/UcelParser/ManualParser/ManualParser.java
@@ -0,0 +1,91 @@
+package org.UcelParser.ManualParser;
+
+import org.Ucel.IProject;
+import org.UcelParser.UCELParser_Generated.UCELLexer;
+import org.UcelParser.UCELParser_Generated.UCELParser;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.ParserRuleContext;
+
+import java.util.ArrayList;
+
+public class ManualParser {
+
+    //region Project
+//    public ParseTree parseProject(IProject project) {
+//        var node = new UCELParser.ProjectContext(null, -1);
+//
+//        var children = new ArrayList<ParseTree>();
+//
+//        children.add(parseProjectDeclaration(node, project.getDeclaration()));
+//        for (var template : project.getTemplates()) {
+//            children.add(parseProjectTemplate(node, template));
+//        }
+//        children.add(parseProjectSystem(node, project.getSystemDeclarations()));
+//
+//        node.children = children;
+//
+//        return node;
+//    }
+
+    //region Project declarations
+
+
+    //endregion
+
+    //region Project template
+
+    //region Graph
+
+
+    //region Locations
+
+    //region Invariant
+
+    //endregion
+
+    //region Exponential
+
+    //endregion
+    //endregion
+
+    //region Edges
+
+    //region Select
+
+    //endregion
+
+    //region Guard
+
+    //endregion
+
+    //region Sync
+
+    //endregion
+
+    //region Update
+
+    //endregion
+    //endregion
+    //endregion
+    //endregion
+
+    //region Project system
+
+    //endregion
+    //endregion
+
+    //region Helper functions
+    private UCELParser generateParser(String input) {
+        CharStream charStream = CharStreams.fromString(input);
+        UCELLexer lexer = new UCELLexer(charStream);
+        CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+        return new UCELParser(tokenStream);
+    }
+
+
+    //endregion
+
+}
