c8956b1dcc7ebf861ddc02686d55db81af465ce9
ChBLA <cbla19@student.aau.dk>
2022-10-28 10:07:49 +0200

    Added project structure to grammar

---
+++ compiler/src/test/java/ParserTests/ManualParsingTests.java
@@ -0,0 +1,54 @@
+package ParserTests;
+
+public class ManualParsingTests {
+
+    //region Project
+
+
+    //region Project declarations
+
+
+    //endregion
+
+    //region Project template
+
+    //region Graph
+
+
+    //region Locations
+
+    //region Invariant
+
+    //endregion
+
+    //region Exponential
+
+    //endregion
+    //endregion
+
+    //region Edges
+
+    //region Select
+
+    //endregion
+
+    //region Guard
+
+    //endregion
+
+    //region Sync
+
+    //endregion
+
+    //region Update
+
+    //endregion
+    //endregion
+    //endregion
+    //endregion
+
+    //region Project system
+
+    //endregion
+    //endregion
+}
