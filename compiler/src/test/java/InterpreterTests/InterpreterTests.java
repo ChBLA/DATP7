aafe88e745ed265cca92cd53709acf99a8e598a5
Henrik Ginnerup <hginnerup@hotmail.com>
2022-11-14 11:36:57 +0100

    Generalized EqExpr tests

--- compiler/src/test/java/InterpreterTests/InterpreterTests.java
+++ compiler/src/test/java/InterpreterTests/InterpreterTests.java
@@ -6,6 +6,7 @@ import org.UcelParser.Util.Scope;
 import org.UcelParser.Util.Value.IntegerValue;
 import org.UcelParser.Util.Value.InterpreterValue;
 import org.UcelParser.Util.Value.BooleanValue;
+import org.UcelParser.Util.Value.StringValue;
 import org.antlr.v4.runtime.RuleContext;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -48,13 +49,13 @@ public class InterpreterTests {
 
     //region eqExpr
     @ParameterizedTest
-    @MethodSource("equalStrings")
-    void eqExprInt(int v0, int v1, boolean expected) {
+    @MethodSource("equalityTestValues")
+    void eqExprTests(InterpreterValue v0, InterpreterValue v1, boolean expected) {
         // Arrange
         var visitor = testVisitor();
 
-        var val0 = mockForVisitorResult(UCELParser.ExpressionContext.class, new IntegerValue(v0), visitor);
-        var val1 = mockForVisitorResult(UCELParser.ExpressionContext.class, new IntegerValue(v1), visitor);
+        var val0 = mockForVisitorResult(UCELParser.ExpressionContext.class, v0, visitor);
+        var val1 = mockForVisitorResult(UCELParser.ExpressionContext.class, v1, visitor);
 
         var node = mock(UCELParser.EqExprContext.class);
         when(node.expression(0)).thenReturn(val0);
@@ -69,17 +70,44 @@ public class InterpreterTests {
 
         assertEquals(expected, actual);
     }
-    private static Stream<Arguments> equalStrings() {
+    private static Stream<Arguments> equalityTestValues() {
         List<Arguments> arguments = new ArrayList<>();
 
+        //region Ints
         for(int i = -3; i<3; i++) {
-            arguments.add(Arguments.of(i,i, true));
+            arguments.add(Arguments.of(value(i), value(i), true));
         }
 
-        arguments.add(Arguments.of(0,1, false));
-        arguments.add(Arguments.of(1,0, false));
-        arguments.add(Arguments.of(-1,1, false));
-        arguments.add(Arguments.of(1,-1, false));
+        arguments.add(Arguments.of(value(0),value(1), false));
+        arguments.add(Arguments.of(value(1),value(0), false));
+        arguments.add(Arguments.of(value(-1),value(1), false));
+        arguments.add(Arguments.of(value(1),value(-1), false));
+        //endregion
+
+        //region Strings
+        arguments.add(Arguments.of(value(""),value(""), true));
+        arguments.add(Arguments.of(value("abc"),value("abc"), true));
+        arguments.add(Arguments.of(value("def"),value("def"), true));
+
+        arguments.add(Arguments.of(value(" "),value(""), false));
+        arguments.add(Arguments.of(value("def "),value("def"), false));
+        arguments.add(Arguments.of(value("abc"),value("def"), false));
+        arguments.add(Arguments.of(value("def"),value("abc"), false));
+        //endregion
+
+        //region Bools
+        arguments.add(Arguments.of(value(true),value(true), true));
+        arguments.add(Arguments.of(value(false),value(false), true));
+        arguments.add(Arguments.of(value(true),value(false), false));
+        arguments.add(Arguments.of(value(false),value(true), false));
+        //endregion
+
+        //region Mixed types
+        arguments.add(Arguments.of(value(0),value(""), false));
+        arguments.add(Arguments.of(value(0),value("0"), false));
+        arguments.add(Arguments.of(value(0),value(false), false));
+        arguments.add(Arguments.of(value(1),value(true), false));
+        //endregion
 
         return arguments.stream();
     }
@@ -97,5 +125,18 @@ public class InterpreterTests {
         var scope = mock(Scope.class);
         return new InterpreterVisitor(scope);
     }
+
+    //region Value
+    private static IntegerValue value(int val) {
+        return new IntegerValue(val);
+    }
+
+    private static StringValue value(String val) {
+        return new StringValue(val);
+    }
+    private static BooleanValue value(boolean val) {
+        return new BooleanValue(val);
+    }
+    //endregion
     //endregion
 }
