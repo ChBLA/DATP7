de9570b257913a8739ea1116fde33102c077117c
Simonbolsen <Simonbolsen1234@gmail.com>
2022-11-21 14:48:19 +0100

    interpreter compVar

--- compiler/src/main/java/org/UcelParser/Util/Value/CompVarValue.java
+++ compiler/src/main/java/org/UcelParser/Util/Value/CompVarValue.java
@@ -12,4 +12,18 @@ public class CompVarValue extends StringValue {
     public int[] getIndices() {
         return this.indices;
     }
+
+    @Override
+    public boolean equals(Object other) {
+        if((other == null || !(other instanceof CompVarValue && super.equals(other)))) return false;
+        CompVarValue v = (CompVarValue) other;
+        if(v.getIndices().length != indices.length) return false;
+
+        for(int i = 0; i < indices.length; i++) {
+            if(indices[i] != v.getIndices()[i]) return false;
+        }
+
+        return true;
+    }
+
 }
