61559780bb81e9eea47e5f9f511df97255602673
SebastianLassen00 <slasse19@student.aau.dk>
2022-11-28 20:07:00 +0100

    Really good code :trollface:

--- compiler/src/main/java/org/UcelParser/CodeGeneration/templates/PowerExprTemplate.java
+++ compiler/src/main/java/org/UcelParser/CodeGeneration/templates/PowerExprTemplate.java
@@ -4,8 +4,18 @@ import org.stringtemplate.v4.ST;
 
 public class PowerExprTemplate extends Template {
     public PowerExprTemplate(Template left, Template right) {
-        //TODO: Implement this method
-        throw new UnsupportedOperationException("Not implemented yet.");
+        //TODO: Implement this method properly, this is just a placeholder / idea
+
+        ST funcCall = new ST("pow(<left>, <right>)");
+        ST funcDef = new ST(
+                "double pow(double base, double exponent) { " +
+                "   double acc = base;" +
+                "   for (i = abs(exponent); i > 1; i--) {" +
+                "       acc *= base;" +
+                "   }" +
+                "return exponent == 0 ? 1 : acc;}"
+        );
+
     }
 }
 
