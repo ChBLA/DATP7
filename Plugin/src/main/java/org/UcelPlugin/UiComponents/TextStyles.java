4470df1141b1eb7331727c5069a7ea8f36b2d95f
Henrik Ginnerup <hginnerup@hotmail.com>
2022-12-07 08:22:02 +0100

    Made warnings darker

--- Plugin/src/main/java/org/UcelPlugin/UiComponents/TextStyles.java
+++ Plugin/src/main/java/org/UcelPlugin/UiComponents/TextStyles.java
@@ -11,6 +11,7 @@ public class TextStyles {
     public final static AttributeSet DEFAULT = CTX.addAttribute(CTX.getEmptySet(), StyleConstants.Foreground, Color.BLACK);
     public final static AttributeSet RED = CTX.addAttribute(CTX.getEmptySet(), StyleConstants.Foreground, Color.RED);
     public final static AttributeSet YELLOW = CTX.addAttribute(CTX.getEmptySet(), StyleConstants.Foreground, Color.YELLOW);
+    public final static AttributeSet DARK_YELLOW = CTX.addAttribute(CTX.getEmptySet(), StyleConstants.Foreground, new Color(126, 126, 0));
     public final static AttributeSet GREEN = CTX.addAttribute(CTX.getEmptySet(), StyleConstants.Foreground, Color.GREEN);
     public final static AttributeSet DARK_GREEN = CTX.addAttribute(CTX.getEmptySet(), StyleConstants.Foreground, new Color(0, 120, 0));
 }
